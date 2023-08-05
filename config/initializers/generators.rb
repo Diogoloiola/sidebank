fbc3711698fefa398b5574c79daf360e02145497
Diogo Loiola <diogomouraloiola@gmail.com>
2023-08-05 09:25:45 -0300

    test: adicionando rspec ao projeto

---
+++ config/initializers/generators.rb
@@ -0,0 +1,14 @@
+Rails.application.config.generators do |g| # rubocop:disable Style/FrozenStringLiteralComment
+  g.test_framework :rspec
+  g.fixture_replacement :factory_bot
+  g.factory_bot dir: 'spec/factories'
+  g.controller_specs false
+  g.request_specs true
+  g.helper_specs false
+  g.feature_specs true
+  g.mailer_specs true
+  g.model_specs true
+  g.observer_specs false
+  g.routing_specs false
+  g.view_specs false
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'database_cleaner'
+require 'shoulda/matchers'
+require 'factory_bot'
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/mocks/**/*.rb')].each { |f| require f }
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+FactoryBot.reload
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
