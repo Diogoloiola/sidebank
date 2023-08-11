8f2f71a61f5f7283ffeb538d8585078c1f26c579
Diogo Loiola <diogomouraloiola@gmail.com>
2023-08-11 13:55:31 -0300

    feat: adicionado modelo do customer ao projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'Customer', at: 'auth/v1/user'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20230811164101_devise_token_auth_create_customers.rb
@@ -0,0 +1,44 @@
+class DeviseTokenAuthCreateCustomers < ActiveRecord::Migration[7.0]
+  def change # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    create_table(:customers) do |t|
+      ## Required
+      t.string :provider, null: false, default: 'email'
+      t.string :uid, null: false, default: ''
+
+      ## Database authenticatable
+      t.string :encrypted_password, null: false, default: ''
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.boolean  :allow_password_change, default: false
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## User Info
+      t.string :name
+      t.string :email
+      t.string :cpf
+      t.string :cellphone
+      t.date :birthdate
+
+      ## Tokens
+      t.json :tokens
+
+      t.timestamps
+    end
+
+    add_index :customers, :email, unique: true
+    add_index :customers, %i[uid provider], unique: true
+    add_index :customers, :reset_password_token, unique: true
+    add_index :customers, :confirmation_token,   unique: true
+    # add_index :customers, :unlock_token,         unique: true
+  end
+end
