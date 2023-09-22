6f9d94fa128a75af22afac3ae2407b27780693fd
Diogo Loiola <diogomouraloiola@gmail.com>
2023-09-22 10:19:24 -0300

    feat: adiciona arquivo seeds ao projeto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+5.times do
+  Customer::Register::Flow.call(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    cpf: CPF.generate,
+    birthdate: Faker::Date.in_date_period(year: 1990, month: 2),
+    cellphone: Faker::PhoneNumber.cell_phone
+  )
+end
+
+Agency::Register::Flow.call(name: Faker::Name.name)
+
+customer = Customer::Record.last
+
+agency = Agency::Record.last
+
+Account::Register::Flow.call(
+  customer_id: customer.id,
+  agency_id: agency.id,
+  account_type: :corrente
+)
